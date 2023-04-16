d916cf17f21253d7f06a363a4e731bb6aeda8f32
OKOLL <47433274+OKOLL@users.noreply.github.com>
2023-04-16 19:38:05 +0200

    added a menu and a chronometer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pyb import ADC, Pin
-from time import time
+from time import time, sleep
 import machine
 import ssd1306
 
@@ -26,6 +26,8 @@ bpm = 0
 last_y = 0
 history = []
 beats = []
+options = ["chronometer","heart rate"]
+selected_option = -1
 
 ################################################################
 # code "init"
@@ -55,8 +57,18 @@ machine.Pin('C3', machine.Pin.OUT).high()
 Pin(Pin.cpu.C2, mode=Pin.IN)
 adc = ADC('C2')
 
+# Buttons
+sw1 = Pin('SW1', Pin.IN)
+sw1.init(Pin.IN, Pin.PULL_UP)
+
+sw2 = Pin('SW2', Pin.IN)
+sw2.init(Pin.IN, Pin.PULL_UP)
+
+sw3 = Pin('SW3', Pin.IN)
+sw3.init(Pin.IN, Pin.PULL_UP)
+
 ################################################################
-# code "runtime"
+# code "bpm"
 ################################################################
 
 # display heart 
@@ -94,12 +106,11 @@ def compute_bpm(beats, previous):
             return (len(beats) / (beat_time)) * 60
     return previous
 
+def menu_bpm():
+    global history
+    global beats
+    global bpm
 
-################################################################
-# code "main"
-################################################################
-
-while(1):
     # get date
     date = rtc.datetime()
     time_display = '{0:02d}'.format(date[4])+':'+'{0:02d}'.format(date[5])+':'+'{0:02d}'.format(date[6])
@@ -119,10 +130,106 @@ while(1):
 
     # a beat must be counted once, hence the verification 
     if value > threshold_beat and history[-1] > history[-2]:
-        print('beat  ' + str(value))
         beats.append(time())
         # Truncate beats queue to max
         beats = beats[-TOTAL_BEATS:]
         bpm = compute_bpm(beats, bpm)
 
     display(time_display, bpm, value, min_value, max_value)
+
+################################################################
+# code "chronometer"
+################################################################
+
+def menu_chronometer():
+    global selected_option
+
+    is_running = False
+    last_time = 0
+    elapsed_time = 0
+    timer = True
+
+    while timer:
+        oled.fill(0) 
+        oled.text("Chronometer", 0, 0)
+
+        if is_running:
+            now = time()
+            elapsed_time += now - last_time
+            last_time = now
+
+        hours = elapsed_time // 3600
+        minutes = (elapsed_time % 3600) // 60
+        seconds = elapsed_time % 60
+        time_str = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+
+        oled.text(time_str, 0, 15)
+        oled.show()
+
+        if sw2.value() == 0:
+            if is_running:
+                # Pause chronometer
+                is_running = False
+            else:
+                # Start chronometer
+                last_time = time()
+                is_running = True
+            sleep(0.2)
+
+        # Reset chronometer
+        elif sw1.value() == 0:
+            elapsed_time = 0
+            is_running = False
+            sleep(0.2)
+
+        elif sw3.value() == 0:
+            timer = False
+            sleep(0.2)
+            
+    selected_option = -1
+
+
+################################################################
+# code "home"
+################################################################
+
+def menu_home():
+    global selected_option
+    global options
+
+    selected_option = 0
+    wait_for_validation = True
+    while wait_for_validation:
+        oled.fill(0)  # Clear the screen
+        oled.text("Menu:", 0, 0)
+
+        for i, option in enumerate(options):
+            if i == selected_option:
+                oled.text("> " + option, 0, 16 + i * 10)
+            else:
+                oled.text(option, 0, 16 + i * 10)
+        oled.show()
+
+        if sw1.value() == 0:
+            selected_option = (selected_option - 1) % len(options)
+            sleep(0.2)
+        elif sw3.value() == 0:
+            selected_option = (selected_option + 1) % len(options)
+            sleep(0.2)
+        elif sw2.value() == 0:
+            oled.fill(0)  
+            sleep(0.2)
+            wait_for_validation = False
+
+################################################################
+# code "main"
+################################################################
+
+while(1):
+    if sw3.value() == 0 or selected_option == -1:
+        sleep(0.2)
+        menu_home()
+    elif selected_option == 0:
+        menu_chronometer()
+    elif selected_option == 1:
+        menu_bpm()
